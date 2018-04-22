c2387790d92d71efd228974da4de0cfab7a20cc2
oriah <oahdout85@gmail.com>
2018-04-22 17:03:32 +0300

    Your Message

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+﻿import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { finalize } from 'rxjs/operators';
 
 import { QuoteService } from './quote.service';
@@ -29,8 +29,8 @@ export class HomeComponent implements OnInit {
       .subscribe((quote: string) => { this.quote = quote; });
 
     $.ajax({
-      // url: "http://eventguests-server.herokuapp.com/wizelyapi1/gettablesstats?event_id=1",
-      url: "http://212.179.232.90:1337/wizelyapi1/gettablesstats?event_id=1",
+       url: "http://eventguests-server.herokuapp.com/wizelyapi1/gettablesstats?event_id=1",
+//      url: "http://212.179.232.90:1337/wizelyapi1/gettablesstats?event_id=1",
       data: null,
       async: false,
       contentType: 'application/json',
