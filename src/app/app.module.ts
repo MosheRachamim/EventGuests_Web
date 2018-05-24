e66fbf1fa2d240dba9c652c67704955b2c81066c
oriah <oahdout85@gmail.com>
2018-05-25 01:23:19 +0300

    Your Message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { TranslateModule } from '@ngx-translate/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { environment } from '@env/environment';
-import { CoreModule } from '@app/core';
-import { SharedModule } from '@app/shared';
-import { HomeModule } from './home/home.module';
-import { AboutModule } from './about/about.module';
-import { LoginModule } from './login/login.module';
+
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { BooksComponent } from './books/books.component';
+import { GuestsArriveInfoComponent } from './guests-arrive-info/guests-arrive-info.component';
+
 
 @NgModule({
+  declarations: [
+    AppComponent,
+    BooksComponent,
+    GuestsArriveInfoComponent
+  ],
   imports: [
-    BrowserModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
-    FormsModule,
-    HttpClientModule,
-    TranslateModule.forRoot(),
-    NgbModule.forRoot(),
-    CoreModule,
-    SharedModule,
-    HomeModule,
-    AboutModule,
-    LoginModule,
-    AppRoutingModule
+    BrowserModule
   ],
-  declarations: [AppComponent],
-  providers: [
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
