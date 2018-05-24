6c699df06a15bb0b96e332a530017d918b2ae73b
oriah <oahdout85@gmail.com>
2018-05-25 01:27:08 +0300

    Your Message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,8 +31,8 @@ export class AppComponent implements OnInit {
     this.isLoading = true;
 
     var data = $.ajax({
-     //        url: "https://eventguests-server.herokuapp.com/wizelyapi1/gettablesstats?event_id=1",
-      url: "http://212.179.232.90:1337/wizelyapi1/gettablesstats?event_id=1",
+             url: "https://eventguests-server.herokuapp.com/wizelyapi1/gettablesstats?event_id=1",
+     // url: "http://212.179.232.90:1337/wizelyapi1/gettablesstats?event_id=1",
       data: null,
       async: false,
       contentType: 'application/json',
